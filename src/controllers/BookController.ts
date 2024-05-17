b5d2b907db5130b789c5d4a3e11be39eba33d4eb
AlvaroSnBz <alvarosnbz@gmail.com>
2024-05-17 21:41:04 +0200

    Added feature to delete book

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -28,3 +28,14 @@ export const setBookStatus = async (req: Request, res: Response) => {
   }
   res.end();
 };
+
+export const deleteBookStatus = async (req: Request, res: Response) => {
+  const id = Array.isArray(req.query.id)
+    ? req.query.id[0]
+    : (req.query.id as string | undefined);
+
+  BookModel.deleteOne({ id })
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
+  res.end();
+};
--- src/routes/BookRoutes.ts
+++ src/routes/BookRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {
+  deleteBookStatus,
   getBook,
   setBookStatus,
 } from '@backend/src/controllers/BookController';
@@ -8,5 +9,6 @@ const bookRouter = express.Router();
 
 bookRouter.get('/getBook', getBook);
 bookRouter.post('/setBookStatus', setBookStatus);
+bookRouter.delete('/deleteBookStatus', deleteBookStatus);
 
 export default bookRouter;
